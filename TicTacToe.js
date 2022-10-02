9f2cd266e4572498cf5d4742e6c9bae0e6f0f1b7
tiahelou10 <tia.elhelou@outlook.com>
2022-10-02 18:22:16 +0300

    Making the computer play

--- TicTacToe.js
+++ TicTacToe.js
@@ -3,16 +3,34 @@ window.onload = () => {
     const cells = document.querySelectorAll(".cell");
 
     let player = 1; // 1 => human and 0 => computer
-    
+
+    let game_state = [2, 2, 2, 2, 2, 2, 2, 2, 2];
+
+    const play = (e) => {
+
+        game_state[e.target.id] = player;
+
+        turn(e.target.id);
+
+        for (let index = 0; index < game_state.length && player == 0; index++) {
+            if (game_state[index] == 2) {
+                game_state[index] = player;
+                turn(index);
+
+            }
+        }
+
+    }
+
     const turn = (id) => {
 
         if (player) {
             cells[id].classList.add("human");
-            player = 0;           
+            player = 0;
         }
         else {
             cells[id].classList.add("computer");
-            player = 1;           
+            player = 1;
         }
     }
 
