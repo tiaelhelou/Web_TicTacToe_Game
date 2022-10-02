d9b91d2ca4b6f5ff3bf7b48ff519dff85a99eda1
tiahelou10 <tia.elhelou@outlook.com>
2022-10-02 18:11:03 +0300

    Switching between two players

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,9 +1,20 @@
 window.onload = () => {
 
+    const cells = document.querySelectorAll(".cell");
+
+    let player = 1; // 1 => human and 0 => computer
+    
     const turn = (id) => {
 
-        cells[id].classList.add("human");
+        if (player) {
+            cells[id].classList.add("human");
+            player = 0;           
+        }
+        else {
+            cells[id].classList.add("computer");
+            player = 1;           
+        }
     }
 
-        cells.forEach(c => c.addEventListener("click", play, false));
+    cells.forEach(c => c.addEventListener("click", play, false));
 }
